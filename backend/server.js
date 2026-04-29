d36791ba9a412de3fd089595458e318d6c6ef5f8
JHANPIER-ROJAS-MACHUCA <rojasmachucajhanpier@gmail.com>
2026-04-29 14:23:09 -0500

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,7 @@ const cors = require("cors");
 
 const app = express();
 
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  "http://localhost:3000"
-].filter(Boolean);
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("No permitido por CORS"));
-    }
-  }
-}));
-
+app.use(cors()); // permite todo
 app.use(express.json());
 
 app.use("/categoria", require("./routes/categoriaRoutes"));
